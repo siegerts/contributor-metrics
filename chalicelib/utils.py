fef54dafaeca28e4f05fb0c758729052282315af
siegerts <stephen.siegert@gmail.com>
2022-05-25 21:11:56 -0400

    add helper for ssm put param

--- chalicelib/utils.py
+++ chalicelib/utils.py
@@ -4,6 +4,18 @@ import boto3
 from botocore.exceptions import ClientError
 
 
+def put_parameter(parameter_name, parameter_value, parameter_type):
+    ssm_client = boto3.client("ssm")
+    try:
+        result = ssm_client.put_parameter(
+            Name=parameter_name, Value=parameter_value, Type=parameter_type
+        )
+    except ClientError as e:
+        logging.error(e)
+        return None
+    return result["Version"]
+
+
 def get_parameter(parameter_name, with_decryption):
     ssm_client = boto3.client("ssm")
     try:
